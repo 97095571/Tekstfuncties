537a9da7782d9ec4a050db2cd39429e520587208
97095571 <117908965+97095571@users.noreply.github.com>
2024-03-07 13:44:57 +0100

    Add files via upload

---
+++ opdr_2.py
@@ -0,0 +1,14 @@
+# Opdracht 1
+# Naam student:
+# Groep:
+
+# Hier komt je code...
+
+my_list = ...
+
+my_string = "Tinus gaat op zijn tandem naar de hottentottententoonstelling"
+
+count_t = my_string.count("t")
+
+
+print(f"De letter 't' komt {count_t}x voor in my_string")
---
+++ opdr_3.py
@@ -0,0 +1,15 @@
+# Opdracht 3 tekstfuncties
+# Naam student:
+# Groep:
+
+# Hier komt je code...
+
+for _ in range(5):
+    print("    *", end='\t')
+    print("   ***", end='\t')
+    print("  ******", end='\t')
+    print(" ********", end='\t')
+    print("***********", end='\t')
+    print("    ***", end='\t')
+    print("    ***", end='\t')
+    print("    ***", end='\t')
